356c4ea74ea7e04f6eacbc3c5da30d4c56df8515
stringnet <roberto@stringnet.pe>
2025-06-19 21:14:00 -0500

    fix(api) import update project and deleteproject

--- api/src/routes/project.routes.js
+++ api/src/routes/project.routes.js
@@ -1,7 +1,9 @@
 // api/src/routes/project.routes.js
 import { Router } from 'express';
-import { getProjects, createProject } from '../controllers/project.controller.js';
 // No necesitamos el middleware aquí porque lo aplicaremos a nivel global en index.js
+// CORRECCIÓN: Se importan todas las funciones necesarias del controlador.
+import { getProjects, createProject, updateProject, deleteProject } from '../controllers/project.controller.js';
+
 
 const router = Router();
 
